ada313564469f90ab963913ca661009d9254f5af
Lei Wang <tendant@gmail.com>
2018-07-26 12:59:05 -0700

    Works no directory

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject selmer-test/child "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [selmer "1.11.8"]]
+  :main ^:skip-aot child.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ src/child/core.clj
@@ -0,0 +1,16 @@
+(ns child.core
+  (:gen-class)
+  (:require [selmer.parser :as parser]
+            [clojure.java.io :as io]))
+
+;; (parser/set-resource-path! (io/resource "templates"))
+
+(defn output
+  []
+  (parser/render-file "demo.html" {}))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World Child!")
+  (println (output)))
